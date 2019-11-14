890e46f3e0dfb98321d1dccad79881879cc3256a
9kongpob <k.kunchanuan@hotmail.com>
2019-11-14 16:24:54 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BasicPart04_SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            string color = "";
+            Console.WriteLine("1 : Car Red");
+            Console.WriteLine("2 : Car Black");
+            Console.WriteLine("3 : Car White");
+            Console.Write("Input your Car Color : ");
+            int.TryParse(Console.ReadLine(),out choice);
+
+            switch (choice)
+            {
+                case 1:
+                    color = "Red";
+                    break;
+                case 2:
+                    color = "Black";
+                    break;
+                case 3:
+                    color = "White";
+                    break;
+                default:
+                    color = "Default";
+                    break;
+            }
+            Console.WriteLine("Color your car : " + color);
+            Console.ReadKey();
+        }
+
+    }
+}
